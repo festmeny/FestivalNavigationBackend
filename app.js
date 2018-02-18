b03ca2ece85dad3320bc53a8feceaaec9f27b8fb
Koncsik Norbert <koncsikn@gmail.com>
2018-02-18 05:54:51 +0100

    View added back for debugging

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var navigation = require('./routes/navigation');
 
 var app = express();
 
+app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
