14ee97cf8a44b852b823ed473807a9b7e0438572
Koncsik Norbert <koncsikn@gmail.com>
2018-03-04 20:51:04 +0100

    In map list, inside is calculated

--- middleware/render/renderMapList.js
+++ middleware/render/renderMapList.js
@@ -1,14 +1,31 @@
+var Geolib = require('geolib');
+
 var Map = require('../../models/map');
 
 module.exports = function(req, res, next){
     var maps = req.custom.maps;
 
-    // TODO: Make valid inside
     var renderedMaps = []
     maps.forEach(element => {
+
+        var inside;
+        if (req.query.lat && req.query.lon){
+            inside = Geolib.isPointInside(
+                {latitude: Number(req.query.lat), longitude: Number(req.query.lon)},
+                [
+                    {latitude: element.bounds.top_left.lat, longitude: element.bounds.top_left.lon},
+                    {latitude: element.bounds.top_left.lat, longitude: element.bounds.bottom_right.lon},
+                    {latitude: element.bounds.bottom_right.lat, longitude: element.bounds.bottom_right.lon},
+                    {latitude: element.bounds.bottom_right.lat, longitude: element.bounds.top_left.lon}
+                ]
+            );
+        }else{
+            inside = false;
+        }
+
         var renderedItem = {
             id: element._id,
-            inside: false,
+            inside: inside,
             name: element.name,
             bounds: {
                 top_left: {
