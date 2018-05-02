e373313e4c7e896f98b45ae127313d115733bced
Koncsik Norbert <koncsikn@gmail.com>
2018-05-02 18:59:17 +0200

    Point crash fix

--- middleware/utils/unifyPointNames.js
+++ middleware/utils/unifyPointNames.js
@@ -1,8 +1,8 @@
 module.exports = function(req, res, next){
     if (req.custom.controls){
-        req.custom.points = controls;
+        req.custom.points = req.custom.controls;
     }else if (req.custom.interests){
-        req.custom.points = interests;
+        req.custom.points = req.custom.interests;
     }
     return next();
 }
